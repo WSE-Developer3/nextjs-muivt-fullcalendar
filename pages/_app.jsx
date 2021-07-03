371c4e3e464f1a7c1d7fd5d08b1d907dd21facb6
Developer_of_Racsche <developer@racsche.com>
2021-07-03 18:01:03 +0900

    fc & muiv5

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,10 +1,43 @@
-import React from "react";
+// import React from "react";
+import * as React from "react";
+import PropTypes from "prop-types";
+import Head from "next/head";
+import { ThemeProvider } from "@material-ui/core/styles";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import theme from "../src/theme";
 
 import "@fullcalendar/common/main.css";
 import "@fullcalendar/daygrid/main.css";
 import "@fullcalendar/timegrid/main.css";
 import "../global-styles.css";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+const cache = createCache({ key: "css" });
+cache.compat = true;
+
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
+
+export default function MyApp(props) {
+  const { Component, pageProps } = props;
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
+
+MyApp.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+  pageProps: PropTypes.object.isRequired,
+};
--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,13 @@ import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import interactionPlugin from "@fullcalendar/interaction";
 import timeGridPlugin from "@fullcalendar/timegrid";
+import Button from "@material-ui/core/Button";
 
 export default () => (
   <>
     <div>
       <button>Hello</button>
+      <Button variant="contained">Hello</Button>
     </div>
     <FullCalendar
       plugins={[interactionPlugin, timeGridPlugin]}
